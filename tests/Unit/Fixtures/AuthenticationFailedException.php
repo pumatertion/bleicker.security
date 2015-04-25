1304721cc01f1c5276a7dbc544d6ac267df5f885
Carsten Bleicker <carsten@bleicker.de>
2015-04-25 18:10:36 +0200

    Require bleicker/authentication

---
+++ tests/Unit/Fixtures/AuthenticationFailedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Bleicker\Security\Unit\Fixtures;
+
+use Bleicker\Security\Exception\AbstractVoterException;
+
+/**
+ * Class AuthenticationFailedException
+ *
+ * @package Tests\Bleicker\Security\Unit\Fixtures
+ */
+class AuthenticationFailedException extends AbstractVoterException {
+
+}
